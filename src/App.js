51e970c287691984eeab183bacd060afb6637fa8
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2021-08-15 20:43:32 +0530

    App.js edited

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import Server from './server';
 import React, { Component } from 'react';
 import Client from './client';
 import 'react-chatbox-component/dist/style.css';
-// const lc=new RTCPeerConnection()
-// const dc=lc.createDataChannel("channel")
-// lc.onicecandidate=e=>console.log("hjgjg "+JSON.stringify(lc.localDescription))
-// lc.createOffer().then(o=>lc.setLocalDescription(o)).then(a=>console.log("set"))
+import {MongoClient} from "mongodb";
+// var c = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://Arishti:18BIT0223@cluster0.bip95.mongodb.net/proj?retryWrites=true&w=majority";
+// const client = new MongoClient(uri);
+    // var url = "mongodb+srv://Arishti:18BIT0223@cluster0.bip95.mongodb.net/proj?retryWrites=true&w=majority";
+// c.connect(url, function(err, db) {
+//  if (err) throw err;
+//  var s = db.db("coldb");
+//  s.collection("col").find().limit(4).toArray(function(err,result) {
+//  if (err) throw err;
+//  console.log(result);
+//  });
+// });
+const lc=new RTCPeerConnection()
+const dc=lc.createDataChannel("channel")
+lc.onicecandidate=e=>console.log("hjgjg "+JSON.stringify(lc.localDescription))
+lc.createOffer().then(o=>lc.setLocalDescription(o)).then(a=>console.log("set"))
 const localConnection = new RTCPeerConnection({ 'iceServers': [{ 'urls': 'stun:74.125.142.127:19302' }] });
 class App extends Component 
 {
